a6c61f550c6f87524da3185815c339a44da89ef8
bdcheung <bdcheung@gmail.com>
2013-04-14 16:26:24 -0400

    Completed all steps up to Authentication

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,16 @@ end
 
 module Rails3DeviseRspecCucumber
   class Application < Rails::Application
+
+    config.generators do |g|
+        g.test_framework :rspec, fixture: true
+        g.fixture_replacement :factory_girl, dir: 'spec/factories'
+        g.view_specs false
+        g.helper_specs false
+        g.stylesheets = false
+        g.javascripts = false
+        g.helper = false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -37,7 +47,7 @@ module Rails3DeviseRspecCucumber
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation]
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
@@ -58,5 +68,5 @@ module Rails3DeviseRspecCucumber
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-  end
+end
 end
